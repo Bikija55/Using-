6c01b4cdef099331d4dbb574e265d7be7695ccbc
RAVI MAKVANA <aaa888aaa99910@gmail.com>
2024-07-24 11:03:19 +0530

    Add files via upload

---
+++ using+cpp.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+             int x,y;
+            
+
+            cout<<"enter first value:";
+            cin>>x;
+            cout<<"enter second value:";
+            cin>>y;
+      // while
+        //   {
+             x=x+y;
+             y=x-y;
+             x=x-y;
+          //  } 
+            
+            cout<<"x is:"<<x<<endl;
+            cout<<"y is:"<<y<<endl;
+                       //         cout<<"value of x:"<<x<<endl;
+   //      cout<<"value of y:"<<y<<endl;
+ 
+    return 0;
+}
